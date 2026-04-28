1b0821042bf5c9215a630a677001145d72324eed
PikachuTW <82265690+PikachuTW@users.noreply.github.com>
2026-04-28 16:52:01 +0800

    Add: open-the-lock - submission-0

---
+++ Data Structures & Algorithms/open-the-lock/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def next_state(self, s: str, digit: int, inc: int) -> str:
+        num = int(s)
+        c = int(s[3 - digit])
+        nc = (c + inc) % 10
+        num -= c * (10 ** digit)
+        num += nc * (10 ** digit)
+        ans = str(num)
+        while len(ans) < 4:
+            ans = "0" + ans
+        return ans
+
+    def openLock(self, deadends: List[str], target: str) -> int:
+        occured = set()
+        q = collections.deque([("0000", 0)])
+        while len(q) != 0:
+            s, depth = q.popleft()
+
+            if s in occured or s in deadends:
+                continue
+
+            occured.add(s)
+
+            if s == target:
+                return depth
+
+            for i in range(4):
+                q.append((self.next_state(s, i, 1), depth + 1))
+                q.append((self.next_state(s, i, -1), depth + 1))
+
+        return -1
