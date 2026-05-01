f26c0dc878f106cba7d58798557f58c1b70983f6
PikachuTW <82265690+PikachuTW@users.noreply.github.com>
2026-05-02 00:38:36 +0800

    Add: ipo - submission-1

---
+++ Data Structures & Algorithms/ipo/submission-1.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+    
+    def __lt__(self, other):
+        return self.val >= other.val
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        n = len(profits)
+        arr = []
+        heap = []
+        for i in range(n):
+            arr.append((capital[i], profits[i]))
+        arr.sort()
+        ai = 0
+        for i in range(k):
+            while ai < n and arr[ai][0] <= w:
+                heapq.heappush(heap, Node(arr[ai][1]))
+                ai += 1
+            if len(heap) > 0:
+                most = heapq.heappop(heap).val
+            else:
+                return w
+            w += most
+        return w
+
+
+        
